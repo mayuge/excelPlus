b4c426054921f67359fd948e8a03246d3b9f0201
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2023-05-31 22:46:58 +0900

    バックアップ
    バックアップ

--- excelPlus.py
+++ excelPlus.py
@@ -5,6 +5,7 @@ import tkinter.scrolledtext as scrolledtext
 from tkinter import messagebox
 import xlwings as xw
 import keyword
+import csv
 
 
 data = []
@@ -51,7 +52,7 @@ def apply_syntax_highlighting(event):
     text.tag_config('user_defined', foreground=user_defined_color)
 
 
-def open_file():
+def open_excel_file():
     filename = filedialog.askopenfilename(title='Open a File', filetype=(('Excel files', '*.xlsx'), ('All Files', '*.*')))
     if filename:
         try:
@@ -59,11 +60,31 @@ def open_file():
             app = xw.App(visible=False)  # Excelアプリケーションを非表示にする場合はvisible=Falseに設定
             workbook = app.books.open(filename)
             fileLabel.config(text=filename+'が読み込まれました')
+            exeLabel.config(text='')
             get_excel(app, workbook)
         except ValueError:
             messagebox.showerror('ファイル破損エラー', 'File could not be opened')
         except FileNotFoundError:
             messagebox.showerror('Error', 'File Not Found')
+
+def open_csv_file():
+    global data
+    filename = filedialog.askopenfilename(title='Open a File', filetype=(('csv files', '*.csv'), ('All Files', '*.*')))
+    with open(filename, 'r', newline='', encoding='shift-jis') as csvfile:
+        #CSVを読み込む
+
+        csvreader = csv.reader(csvfile)
+        #SBVをリスト化
+        data = list(csvreader)
+        fileLabel.config(text=filename+'が読み込まれました')
+        exeLabel.config(text='')
+
+def open_py_file():
+    filename = filedialog.askopenfilename(title='Open a File', filetype=(('py files', '*.py'), ('All Files', '*.*')))
+    with open(filename, 'r',encoding='utf-8') as pyfile:
+        pycode = pyfile.read()
+        text.insert(2.0,pycode)
+        apply_syntax_highlighting(0)
 
 def get_excel(app, workbook):
     global data
@@ -108,17 +129,40 @@ def save_to_excel():
             workbook = app.books.add()
             sheet = workbook.sheets.active
             #print(data)
-            xw.Range("A1").options(ndim=2, empty='').value = data
+            #xw.Range("A1").options(ndim=2, empty='').value = data
+            for i, row in enumerate(data):
+                for j, value in enumerate(row):
+                    cell = sheet.cells(i+1, j+1)
+                    cell.value = value
             # ブックを保存
             workbook.save(filename)
 
             # Excelアプリケーションを終了
             app.quit()
-
             messagebox.showinfo('Success', 'データが正しく格納されました')
         except Exception as e:
             messagebox.showerror('エクセル出力時エラー', str(e))
+        
+            
 
+def save_to_csv():
+    global data
+    filename = filedialog.asksaveasfilename(title='Save as csv', defaultextension='.csv', filetypes=(('csv files', '*.csv'), ('All Files', '*.*')))
+    with open(filename, 'w', newline='', encoding='shift-jis') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        try:
+            csvwriter.writerows(data)
+        finally:
+            messagebox.showinfo('Success', 'データが正しく格納されました')
+
+def save_py_file(text):
+    filename = filedialog.asksaveasfilename(title='Save as py', defaultextension='.py', filetypes=(('py files', '*.py'), ('All Files', '*.*')))
+    with open(filename, 'w', encoding='utf-8') as pyfile:
+        try:
+            pycode = text.get('1.0', 'end-1c')
+            pyfile.write(pycode)
+        finally:
+            messagebox.showinfo('Success', 'データが正しく格納されました')
 
 root = tk.Tk()
 root.title('excelPlus')
@@ -128,18 +172,25 @@ m = tk.Menu(root)
 root.config(menu=m)
 file_menu = tk.Menu(m, tearoff=False)
 m.add_cascade(label='メニュー', menu=file_menu)
-file_menu.add_command(label='エクセルを読み込む', command=open_file)
-file_menu.add_command(label='実行結果を保存', command=save_to_excel)
+file_menu.add_command(label='エクセルを読み込む', command=open_excel_file)
+file_menu.add_command(label='csvを読み込む', command=open_csv_file)
+file_menu.add_command(label='実行結果をエクセルで保存', command=save_to_excel)
+file_menu.add_command(label='実行結果をcsvで保存', command=save_to_csv)
+file_menu.add_command(label='pyファイルを読み込む', command=open_py_file)
+
 
 fileLabel = tk.Label(text='ファイルが読み込まれていません')
 fileLabel.pack()
 
 # コード入力欄
 text = scrolledtext.ScrolledText(root, height=10)
-text.insert('1.0','#ここではpythonが記述でき、読み込んだエクセルは2次元配列dataとして格納されています。メニューからデータを選択して、print(data)してみましょう')
+text.insert('1.0','#ここではpythonが記述でき、読み込んだエクセル、csvは2次元配列dataとして格納されています。メニューからデータを選択して、print(data)してみましょう\n')
 apply_syntax_highlighting(0)
 text.bind('<KeyRelease>', apply_syntax_highlighting)
 text.pack(fill=BOTH, expand=True)
+
+file_menu.add_command(label='pyファイルの保存', command=lambda: save_py_file(text))
+
 
 # Create a button to execute the code
 exeLabel = tk.Label(text='')
